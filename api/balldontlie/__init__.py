10f1fe83b4fb10adb9342041a08d37e32be0afe8
sam453 <sam@obsessvr.com>
2022-03-20 16:47:23 -0400

    adding blueprint.py and routes for balldontlieapi

---
+++ api/balldontlie/__init__.py
@@ -0,0 +1,5 @@
+"""
+Routes module for /balldontlie
+"""
+from . import routes
+from . import blueprint
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return "Welcome to the Quizerator"
 
 if __name__ == "__main__":
   app.run()
