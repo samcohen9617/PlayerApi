d2ce4d4d523bf7d8396692e010ffb6be120aab1e
sam453 <sam@obsessvr.com>
2022-03-20 17:44:17 -0400

    switching to nba stuffer

--- app/api/balldontlie/controllers.py
+++ app/api/balldontlie/controllers.py
@@ -1,3 +1,8 @@
+import requests as reqs
+import app.data_manipulation.bs4_functions as scraper
 
 def get_all_current_players_stats():
-    return "Players"
+    nba_stuffer_url = "https://www.nbastuffer.com/2021-2022-nba-player-stats/"
+
+    response = reqs.get(nba_stuffer_url)
+    return scraper.nba_stuffer_player_stats_to_json(response.text)
---
+++ app/data_manipulation/bs4_functions.py
@@ -0,0 +1,4 @@
+import bs4
+
+def nba_stuffer_player_stats_to_json(page_html):
+    return page_html
