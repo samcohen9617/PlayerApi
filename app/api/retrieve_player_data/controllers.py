a4fcc4c4a9bf3da311ae468a98c86e253e944c88
sam453 <sam@obsessvr.com>
2022-03-26 22:33:17 -0400

    get players route is complete

--- app/api/retrieve_player_data/controllers.py
+++ app/api/retrieve_player_data/controllers.py
@@ -5,4 +5,4 @@ def get_all_current_players_stats():
     nba_stuffer_url = "https://www.nbastuffer.com/2021-2022-nba-player-stats/"
 
     response = reqs.get(nba_stuffer_url)
-    return scraper.nba_stuffer_player_stats_to_json(response.text)
+    return scraper.nba_stuffer_player_stats_to_df(response.text)
--- app/data_manipulation/bs4_functions.py
+++ app/data_manipulation/bs4_functions.py
@@ -1,4 +1,20 @@
-import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
 
-def nba_stuffer_player_stats_to_json(page_html):
-    return page_html
+
+def nba_stuffer_player_stats_to_df(page_html):
+    soup = BeautifulSoup(page_html, 'html.parser')
+    tbl = soup.find("table")
+    column_names = []
+    for th in tbl.find('thead').find('tr').find_all('th'):
+        if th.find('a'):
+            a = th.find('a')
+            for span in a.find_all('span'):
+                span.decompose()
+            column_names.append(a.text)
+        else:
+            column_names.append(th.text)
+    df = pd.read_html(str(tbl))[0].transpose()
+    df = df.drop('RANK', 1)
+
+    return df.to_json()
